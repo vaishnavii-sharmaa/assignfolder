8308af5d2bbbdd38ba345beded96bd3bd3911f27
vaishnavi sharma <vaishnavi.sharma_cs23@gla.ac.in>
2025-09-02 09:06:52 +0530

    added assignmentsept folder

---
+++ assignmentsept/Que5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+import javax.sql.rowset.spi.SyncResolver;
+public class Que5 {
+    Scanner sc=new Scanner(System.in);
+    double k=sc.nextInt();
+    double  Miles = K* 0.621371;
+    System.out.print(Miles);
+    
+}
